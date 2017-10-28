b2f2c5c3d59ba405a5ff36cf21fa6284fa1325ff
zy1937 <18692380598@163.com>
2017-10-28 20:09:06 +0800

    add some code to getDataDromSSE

--- getDataFromSSE.py
+++ getDataFromSSE.py
@@ -1,8 +1,23 @@
 #-*- coding:utf-8 -*-
-import requests
+import urllib3
+import urllib
 
-url = 'http://www.baidu/com'
-r = requests.get(url)
+
+url = 'http://www.sse.com.cn/market/stockdata/dividends/dividend/'
+headers = { "Accept":"text/html,application/xhtml+xml,application/xml;",
+            "Accept-Encoding":"gzip",
+            "Accept-Language":"zh-CN,zh;q=0.8",
+            "Referer":"http://www.example.com/",
+            "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
+            }
+
+f = urllib.urlopen(url)
+
+htmlData = f.read()
+
+
+
+
 print("-----begin to get -------")
-print(r.text)
+print(htmlData)
 
